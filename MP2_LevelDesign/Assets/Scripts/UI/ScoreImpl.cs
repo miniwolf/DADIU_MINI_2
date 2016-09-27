92fa498590db09a34d3782ccfbfd3b4c03ed74d7
Sindri <s151200@student.dtu.dk>
2016-09-27 17:16:37 +0200

    Controllers for menu and score added. Implementations are not done. MonoBehavior of PlayerImpl.cs deleted.

---
+++ MP2_LevelDesign/Assets/Scripts/UI/ScoreImpl.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ScoreImpl : ScoreController {
+	private Player player;
+	private Life life;
+
+	public void ShowLife() {
+		life = player.GetLife();
+		// TODO show life on UI element
+	}
+
+	public void ShowPoints() {
+		throw new NotImplementedException();
+	}
+}
