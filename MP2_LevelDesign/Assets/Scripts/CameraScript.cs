a4064b78cdf27cffb848ef9d4ba42f3c283c53ff
Sindri <s151200@student.dtu.dk>
2016-09-27 14:43:10 +0200

    Camera script added

---
+++ MP2_LevelDesign/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	private GameObject player;
+	private Vector3 cameraPosition;
+	private float offset = 12f;
+	public float cameraDelayTime = 0.5f;
+
+	// Use this for initialization
+	void Start() {
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	// Update is called once per frame
+	void Update() {
+		StartCoroutine(DelayMove());
+	}
+
+	IEnumerator DelayMove() {
+		Vector3 playerPos = player.transform.position;
+		
+		yield return new WaitForSeconds(cameraDelayTime);
+		//cameraPosition = new Vector3(player.transform.position.x - offset, offset, player.transform.position.z - offset);
+		//transform.position = cameraPosition;
+		transform.position = new Vector3(playerPos.x - offset, offset, playerPos.z - offset);
+
+	}
+}
