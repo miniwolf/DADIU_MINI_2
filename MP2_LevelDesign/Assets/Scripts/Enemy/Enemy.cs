963cd4189626a6b7205ae057ab3d8e1a9f63c061
Laura <lauryxky@hotmail.com>
2016-09-27 14:05:33 +0200

    Implemmented Enemy and EnemyState classes

---
+++ MP2_LevelDesign/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	private EnemyState state;
+	private NavMeshController navmesh;
+	//private AnimationController animation;
+	//private SoundController sound;
+
+	public Enemy(NavMeshAgent agent) {
+		navmesh = new NavMeshController(agent);
+		state = EnemyState.RandomWalk;
+		// set animation and sound controller
+	}
+
+	public EnemyState GetState () {
+		return state;
+	}
+
+	public void SetState (EnemyState newState) {
+		//update state
+		state = newState;
+	}
+}
