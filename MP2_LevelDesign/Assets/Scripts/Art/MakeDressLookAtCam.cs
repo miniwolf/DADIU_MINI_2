35930614b0467576e845bf3484d9dcd7b951338e
Christopher <cnelson.private@gmail.com>
2016-09-29 13:01:58 +0200

    Added Troll And Dress

---
+++ MP2_LevelDesign/Assets/Scripts/Art/MakeDressLookAtCam.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MakeDressLookAtCam : MonoBehaviour {
+
+	GameObject cam;
+
+	// Use this for initialization
+	void Start () {
+		cam = GameObject.FindGameObjectWithTag(TagConstants.CAMERA);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt (new Vector3(cam.transform.position.x,transform.rotation.y,cam.transform.position.z));
+		transform.Rotate (new Vector3(0,180f,0));
+	}
+}
