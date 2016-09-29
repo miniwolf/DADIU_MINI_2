d722a87736dea5988f4c451aa1499ac78bf1bce6
Laura <lauryxky@hotmail.com>
2016-09-29 16:45:09 +0200

    Yellow bush added changing the state of the troll and slowing it down

---
+++ MP2_LevelDesign/Assets/Scripts/items/command/YellowBushCommand.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class YellowBushCommand : ItemCommand {
+	private Enemy enemy;
+	private GameObject yellowBushObject;
+
+	public YellowBushCommand(Enemy enemy) {
+		this.enemy = enemy;
+	}
+
+	/// <summary>
+	/// When starting a yellow bush should be kinematic
+	/// </summary>
+	/// <param name="gameObject">Game object of the bridge</param>
+	public void Setup(GameObject gameObject) {
+		this.yellowBushObject = gameObject;
+	}
+
+	/// <summary>
+	/// When hitting the enemy we toggle its state as collider
+	/// </summary>
+	/// <param name="other">Colliding object</param>
+	public void Execute(Collider other) {
+		if ( other.transform.tag == TagConstants.ENEMY ) {
+			enemy.SetState(EnemyState.ObstacleHit);
+		}
+	}
+}
