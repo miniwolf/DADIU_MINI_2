49d09b85eaa42eeeb243ee3fadda24acc5470038
Nicolai Hansen <nelkjaerhansen@gmail.com>
2016-09-30 10:59:13 +0200

    Added prefabs for Wwise
    Initial sound of troll footsteps and other stuff.

---
+++ MP2_LevelDesign/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,56 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID AUNTIEFOOTSTEP = 3500283897U;
+        static const AkUniqueID DRESSPICKUP = 2867536338U;
+        static const AkUniqueID MENUBACKWARDCLICK = 3139869105U;
+        static const AkUniqueID MENUFORWARDCLICK = 4028748541U;
+        static const AkUniqueID STOPAUNTIEDRESSPICKUPVOICE = 1422264338U;
+        static const AkUniqueID STOPAUNTIEFOOTSTEPS = 3527026064U;
+        static const AkUniqueID STOPTROLLFOOTSTEPS = 1093813131U;
+        static const AkUniqueID TROLLCATCHAUNTIE = 3227179805U;
+        static const AkUniqueID TROLLFALLSINTOWATER = 1919802259U;
+        static const AkUniqueID TROLLFOOTSTEP = 835295888U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN_SOUNDBANK = 2228651116U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
