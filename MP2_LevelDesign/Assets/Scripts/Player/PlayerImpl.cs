67237fbae63f571578b10bfc8ffe395da0e3b8af
Sindri <s151200@student.dtu.dk>
2016-10-03 13:28:53 +0200

    TapFeedback prefab and material added. PlayerImpl.cs modified to support tap feedback.

--- MP2_LevelDesign/Assets/Scripts/Player/PlayerImpl.cs
+++ MP2_LevelDesign/Assets/Scripts/Player/PlayerImpl.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System.Collections;
 
 public class PlayerImpl : MonoBehaviour, Player, GameEntity, Controllable {
 	private RaycastHit hit;
@@ -9,6 +10,9 @@ public class PlayerImpl : MonoBehaviour, Player, GameEntity, Controllable {
 
 	private PlayerState playerState;
 	private List<Controller> controllers = new List<Controller>();
+	private GameObject tapFeedback;
+	private Renderer rend;
+	private Color color;
 
 	void Awake() {
 		InjectionRegister.Register(this);
@@ -17,29 +21,38 @@ public class PlayerImpl : MonoBehaviour, Player, GameEntity, Controllable {
 
 	void Start() {
 		cam = GameObject.FindGameObjectWithTag(TagConstants.CAMERA).GetComponent<Camera>();
+		tapFeedback = GameObject.FindGameObjectWithTag(TagConstants.TAP_FEEDBACK);
+		rend = tapFeedback.GetComponent<Renderer>();
 	}
 
 	public void SetupComponents() {
 	}
-		
+
 	void Update() {
-		foreach(Touch touch in Input.touches) {
+		foreach (Touch touch in Input.touches) {
 			cameraToGround = cam.ScreenPointToRay(touch.position);
-			if ( Physics.Raycast(cameraToGround,out hit,500f,layerMask.value) ) {
-				foreach ( Controller controller in controllers ) {
+			if (Physics.Raycast(cameraToGround, out hit, 500f, layerMask.value)) {
+				foreach (Controller controller in controllers) {
 					controller.Move(hit.point);
 				}
 			}
 		}
 
-		if ( Input.GetMouseButtonDown(1) ) {
+		// Mouse for debugging on a PC
+		if (Input.GetMouseButtonDown(1)) {
 			cameraToGround = cam.ScreenPointToRay(Input.mousePosition);
-			if ( Physics.Raycast(cameraToGround, out hit,500f,layerMask.value) ) {
-				foreach ( Controller controller in controllers ) {
+			if (Physics.Raycast(cameraToGround, out hit, 500f, layerMask.value)) {
+				foreach (Controller controller in controllers) {
 					controller.Move(hit.point);
+					TapFeedback(hit);
 				}
 			}
 		}
+	}
+
+	private void TapFeedback(RaycastHit hit) {
+		tapFeedback.transform.position = hit.point;
+		rend.material.color = Color.green;
 	}
 
 	public void SetState(PlayerState newState) {
