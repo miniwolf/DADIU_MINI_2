0f2192a362bbbfbb17b70d7e8309d617b19ad92a
blacou12 <blacou12@student.aau.dk>
2016-10-03 10:51:04 +0200

    Gave LD vars for troll chase, and max speed

---
+++ MP2_LevelDesign/Assets/Scripts/UI/FloatingNumber/FloatingNumberFeedback.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FloatingNumberFeedback : MonoBehaviour, Value {
+
+	private float dressesPickedUp = 0;
+	public float secondsUntilDisappear = 2.5f;
+	private Coroutine displayNumber;
+	private bool displayNumberRunning = false;
+	private Text text;
+
+	// Use this for initialization
+	void Start () {
+		TagRegister.RegisterSingle(this.gameObject, TagConstants.FEEDBACKNUMBER);
+		text = GameObject.FindGameObjectWithTag(TagConstants.FEEDBACKNUMBER).GetComponent<Text>();
+		gameObject.SetActive(false);
+	}
+
+	public void IncrementValue(){
+		dressesPickedUp++;
+		ShowNumber();
+	}
+
+	public void DecrementValue(){
+		dressesPickedUp--;
+	}
+
+	public float GetValue(){
+		return dressesPickedUp;
+	}
+
+	private void ResetCurrDressCount(){
+		dressesPickedUp = 0f;
+	}
+		
+	private void ShowNumber(){
+		if (displayNumberRunning) {
+			StopCoroutine(displayNumber);
+		}
+		gameObject.SetActive(true);
+		displayNumber = StartCoroutine(FunctionalityOfNumber(GetValue()));
+	}
+
+	IEnumerator FunctionalityOfNumber(float amount){
+		displayNumberRunning = true;
+		text.text = amount.ToString();
+		//Play animation
+		yield return new WaitForSeconds(secondsUntilDisappear);
+		gameObject.SetActive(false);
+		ResetCurrDressCount();
+		displayNumberRunning = false;
+	}
+}
