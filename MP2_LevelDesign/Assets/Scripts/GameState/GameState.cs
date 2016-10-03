d1541bf6a0427c5f12481b833fea2ee9f1f0c7d3
blacou12 <blacou12@student.aau.dk>
2016-10-03 15:45:57 +0200

    End game functionalities should be working

--- MP2_LevelDesign/Assets/Scripts/GameState/GameState.cs
+++ MP2_LevelDesign/Assets/Scripts/GameState/GameState.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class GameState {
 	public class Playing : GameStates {
@@ -10,6 +11,18 @@ public class GameState {
 	public class Paused : GameStates {
 		public void Execute() {
 			Time.timeScale = 0.0f;
+		}
+	}
+
+	public class Ended : GameStates {
+		EndGame endGame;
+
+		public Ended(){
+			endGame = GameObject.FindGameObjectWithTag("GameEnd").GetComponent<EndGame>();
+		}
+
+		public void Execute(){
+			endGame.BeginFade();
 		}
 	}
 }
