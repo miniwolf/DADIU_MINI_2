d77bb8f856399466a013e3281b9ffb5209aadfd0
Martin <martinvano00@gmail.com>
2016-10-03 11:43:42 +0200

    New methods in animations controller

--- MP2_LevelDesign/Assets/Scripts/Player/controller/PlayerAnimControllerImpl.cs
+++ MP2_LevelDesign/Assets/Scripts/Player/controller/PlayerAnimControllerImpl.cs
@@ -1,26 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
+/// <summary>
+/// This object needs to be attached to Player object in the game!
+/// </summary>
+public class PlayerAnimControllerImpl : MonoBehaviour, PlayerAnimController {
 
-public class PlayerAnimControllerImpl : PlayerAnimController {
+	private const string BOOL_IS_MOVING = "isMoving";
+	private const string BOOL_IS_THROWN = "isThrown";
+	private const string TRIGGER_GET_UP = "getUp";
+	private const string TRIGGER_CAUGHT = "caught";
+
 	private Animator animator;
 
-	public PlayerAnimControllerImpl(Animator animator) {
-		this.animator = animator;
+	void Start() { 
+		animator = GetComponent<Animator>();	
 	}
 
 	public void PickUpItem() {
-	
+		// todo no animation
 	}
 
 	public void Idle() {
-	
+		animator.SetBool(BOOL_IS_MOVING, false);
 	}
 
 	public void Move(Vector3 moveTo) {
-	
+		animator.SetBool(BOOL_IS_MOVING, true);
 	}
 
 	public void HitObstacle(GameObject obstacle) {
-	
+		// no animation
 	}
+
+	public void Caught() {
+		Idle();
+
+		animator.SetTrigger(TRIGGER_CAUGHT);
+		log("caught");
+		animator.SetBool(BOOL_IS_THROWN, true);
+		log("thrown true");
+		StartCoroutine(ContinueIdleCoroutine());
+	}
+
+	private IEnumerator ContinueIdleCoroutine() {
+		yield return StartCoroutine(WakeUpCoroutine());
+		animator.SetBool(BOOL_IS_MOVING, true);
+	}
+
+	private IEnumerator WakeUpCoroutine() {
+		animator.SetTrigger(TRIGGER_GET_UP);
+		log("getUp start");
+		yield return  new WaitForSeconds(1f);
+	}
+
+	private void log(string msg) {
+		Debug.Log(msg);
+	}
+
 }
