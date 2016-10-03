ea58ac6e6c6b0398cb5651f5cdcb62eeba29509b
Alyn <alyn_509@yahoo.com>
2016-10-03 15:53:37 +0200

    added LifeCommander

---
+++ MP2_LevelDesign/Assets/Scripts/Player/commands/LifeCommander.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class LifeCommander : MovableCommand {
+    InGameController inGameController;
+    GameStateManager gameStateManager;
+    Player player;
+    Enemy enemy;
+
+    public LifeCommander(Player player, Enemy enemy, GameStateManager gameStateManager, InGameController inGameController) {
+        this.player = player;
+        this.enemy = enemy;
+        this.gameStateManager = gameStateManager;
+        this.inGameController = inGameController;
+    }
+
+    public void Execute(Collider other) {
+        if ( other.tag != TagConstants.ENEMY ) {
+            return;
+        }
+        if (player.GetState() != PlayerState.Idle) {
+            if (inGameController.GetLifeValue() >= 0) {
+                player.SetState(PlayerState.Idle);
+                inGameController.DecrementLife();
+            } else {
+                player.SetState(PlayerState.Dead);
+                gameStateManager.NewState(new GameState.End());
+            }
+            enemy.SetState(EnemyState.GirlCaught);
+        }
+    }
+}
